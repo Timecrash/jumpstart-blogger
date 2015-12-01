20caa41ce43410a7251bcf26bb3e8e965f8b8016
Matt Velez <mzvelez75@gmail.com>
2015-11-30 19:54:14 -0500

    Completed L1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -6,4 +8,38 @@ class ArticlesController < ApplicationController
   def show
     @articles = Article.find(params[:id])
   end
+
+  def new
+    @articles = Article.new
+  end
+
+  def edit
+    @articles = Article.find(params[:id])
+  end
+
+  def create
+    @articles = Article.create(article_params)
+
+    flash.notice = "Article '#{@articles.title}' created!"
+
+    redirect_to article_path(@articles)
+  end
+
+  def update
+    @articles = Article.find(params[:id])
+    @articles.update(article_params)
+
+    flash.notice = "Article '#{@articles.title}' updated!"
+
+    redirect_to article_path(@articles)
+  end
+
+  def destroy
+    @articles = Article.find(params[:id]).destroy
+
+    flash.notice = "Article '#{@articles.title}' deleted!"
+
+    redirect_to articles_path
+  end
+
 end
